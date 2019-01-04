d9f549389a3e2e62b0e0dddda42a73f4cad2476f
ZHI-XINHUA <1960881192@qq.com>
2019-01-04 13:50:57 +0800

    初始化

---
+++ src/main/java/zxh/TestController.java
@@ -0,0 +1,18 @@
+package zxh;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 测试
+ */
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        System.out.println("zzz");
+        return "hello";
+    }
+}
